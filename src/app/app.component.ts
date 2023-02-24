1a7eced26593abf2f43f7d36beb769e32da18bd4
Daniel Prieto <88695568+danobanano88@users.noreply.github.com>
2023-02-24 16:27:08 -0600

    almost

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,21 @@
 import { Component } from '@angular/core';
 
+export interface PeriodicElement {
+  name: string;
+  id: number;
+  lastname: string;
+  genre: string;
+  //add short date type
+  dob: Date
+
+}
+
+let ELEMENT_DATA: PeriodicElement[] = [
+  {id: 1, name: 'Daniel', lastname: "Prieto Huguenin", genre: 'H', dob: new Date('1998-11-18')}, 
+  {id: 2, name: 'Juan', lastname: "Perez", genre: 'H', dob: new Date('1998-11-18')},
+  {id: 3, name: 'Maria', lastname: "Perez", genre: 'M', dob: new Date('1998-11-18')},
+  {id: 4, name: 'Pedro', lastname: "Perez", genre: 'H', dob: new Date('1998-11-18')},
+];
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +23,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'crudColegio';
+  displayedColumns: string[] = ['id', 'name', 'lastname', 'genre' , 'dob', 'delete'];
+  dataSource = ELEMENT_DATA;
+
+  delete(id: number) {
+    this.dataSource = this.dataSource.filter((value,key)=>{
+      return value.id != id;
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDialogModule} from '@angular/material/dialog';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    MatSlideToggleModule,
+    MatTabsModule,
+    MatCardModule,
+    MatTableModule,
+    MatIconModule,
+    MatButtonModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
